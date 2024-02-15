227aecdc59f76a14b5b0ad8b983382d284084dc8
swaleh <ethereumdevelopment@gmail.com>
2024-02-15 08:14:54 +0000

    Dashboard put in place

--- finance-manager/src/App.tsx
+++ finance-manager/src/App.tsx
@@ -1,20 +1,10 @@
-import { useState } from "react";
-import "./App.module.scss";
+import "./App.scss";
+import { Dashboard } from "./components/Dashboard/Dashboard";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
+            <Dashboard />
         </>
     );
 }
---
+++ finance-manager/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,21 @@
+import "./Dashboard.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoneyBillTrendUp } from "@fortawesome/free-solid-svg-icons";
+
+export const Dashboard = () => {
+    return (
+        <header>
+            <div className="Dash-UI">
+                <div className="logo">
+                    <FontAwesomeIcon
+                        icon={faMoneyBillTrendUp}
+                        style={{ fontSize: "3rem" }}
+                    />
+                </div>
+                <div className="content">
+                    <h1>Dashboard</h1>
+                </div>
+            </div>
+        </header>
+    );
+};
